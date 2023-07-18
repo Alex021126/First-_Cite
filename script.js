49ed6ab8211170d537da210cdcd67f4bde5730a6
Alex021126 <2385920112@qq.com>
2023-07-18 17:10:32 +0800

    7.18

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const list = document.createElement('ul');
 const info = document.createElement('p');
 const html = document.querySelector('html');
 
-info.textContent = 'Below is a dynamic list. Click anywhere on the page to add a new list item. Click an existing list item to change its text to something else.';
+info.textContent = 'Below is a dynamic list. Click here to add a new list item. Click an existing list item to change its text to something else.';
 
 document.body.appendChild(info);
 document.body.appendChild(list);
 
-html.onclick = function() {
+info.onclick = function() {
   const listItem = document.createElement('li');
   const listContent = prompt('What content do you want the list item to have?');
   listItem.textContent = listContent;
@@ -19,3 +19,24 @@ html.onclick = function() {
     this.textContent = listContent;
   }
 }
+ /*以250px显示左边栏*/
+ function openNavleft() {
+  document.getElementById("leftside").style.display="block";
+  document.getElementById("leftside").style.width = "250px";
+}
+/*隐藏左边栏*/
+function closeNavleft() {
+  document.getElementById("leftside").style.display="none";
+}
+ /*以250px显示右边栏*/
+ function openNavright() {
+  document.getElementById("rightside").style.display="block";
+  document.getElementById("rightside").style.width = "250px";
+}
+/*隐藏右边栏*/
+function closeNavright() {
+  document.getElementById("rightside").style.display="none";
+}
+
+
+
